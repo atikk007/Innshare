f80838022ec68370954240b3bdf37ea1e0860f17
Atik Mansuri <atikmansuri45@gmail.com>
2022-09-25 10:49:25 +0530

    added route array

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 // Cors
 
 const corsOptions = {
-    origin: process.env.ALLOWED_CLIENTS.split(',')
+    origin: [process.env.ALLOWED_CLIENTS.split(',')]
 }
 
 app.use(cors(corsOptions));
