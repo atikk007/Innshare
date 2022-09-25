f62bf390ea8705d94ea61a892bc0ea31facdc0db
Atik Mansuri <atikmansuri45@gmail.com>
2022-09-25 16:09:15 +0530

    minor change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 // Cors
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+    origin: ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300', 'https://inshare-ak.herokuapp.com/']
 }
 
 app.use(cors(corsOptions));
