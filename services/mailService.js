4adb159e00d29d9f2cec874952ccaa22be17f003
Atik Mansuri <atikmansuri45@gmail.com>
2022-09-25 14:07:41 +0530

    .

--- services/mailService.js
+++ services/mailService.js
@@ -13,7 +13,7 @@ module.exports = async ({ from, to, subject, text, html }) => {
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
-        from: `inShare <${from}>`, // sender address
+        from: 'inShare <atikmansuri619@gmail.com>', // sender address
         to: to, // list of receivers
         subject: subject, // Subject line
         text: text, // plain text body
