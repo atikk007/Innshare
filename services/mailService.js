af1299aca0896e99809602ed8f3d78a5302cf29a
Atik Mansuri <atikmansuri45@gmail.com>
2022-09-25 13:57:49 +0530

    test update

--- services/mailService.js
+++ services/mailService.js
@@ -1,4 +1,5 @@
 const nodemailer = require("nodemailer");
+const dotenv = require('dotenv').config();
 module.exports = async ({ from, to, subject, text, html }) => {
     let transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
@@ -6,7 +7,7 @@ module.exports = async ({ from, to, subject, text, html }) => {
         secure: false, // true for 465, false for other ports
         auth: {
             user: process.env.MAIL_USER, // generated ethereal user
-            pass: process.env.MAIL_PASSWORD, // generated ethereal password
+            pass: process.env.MAIL_PASS, // generated ethereal password
         },
     });
 
