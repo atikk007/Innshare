bdc48816434422b12bc83ae97ca842fba3729278
Atik Mansuri <atikmansuri45@gmail.com>
2022-10-02 09:19:52 +0530

    deployed on cyclic

--- public/index.js
+++ public/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://inshare-ak.herokuapp.com";
+const baseURL = "https://inshare.cyclic.app";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
